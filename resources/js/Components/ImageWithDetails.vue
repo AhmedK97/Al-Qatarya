2a59e524da4a814a4df1a1c9d4db1ab482025d3d
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2023-08-29 19:44:36 +0300

    fix image mouse event

--- resources/js/Components/ImageWithDetails.vue
+++ resources/js/Components/ImageWithDetails.vue
@@ -1,18 +1,13 @@
 <template>
-    <div class="relative">
-        <img
-            :src="imageSrc"
-            :alt="altText"
-            class="object-cover w-full h-auto duration-300 rounded-lg cursor-pointer hover:opacity-80"
-            @mouseover="showDetails = true"
-            @mouseleave="showDetails = false"
-        />
+    <div class="relative"
+         @mouseover="showDetails = true"
+         @mouseleave="showDetails = false">
+        <img :src="imageSrc" :alt="altText"
+            class="object-cover w-full h-auto duration-300 rounded-lg cursor-pointer hover:opacity-80" />
 
         <!-- Image details -->
-        <div
-            v-if="showDetails"
-            class="absolute inset-0 flex items-center justify-center text-white bg-gray-900 rounded-lg"
-        >
+        <div v-if="showDetails"
+            class="absolute inset-0 flex items-center justify-center text-white bg-gray-900 rounded-lg">
             <div class="text-center">
                 <h3 class="text-lg font-semibold">{{ title }}</h3>
                 <p class="mt-2">{{ description }}</p>
@@ -22,16 +17,18 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-defineProps({
-    imageSrc: String,
-    altText: String,
-    title: String,
-    description: String,
-});
-const showDetails = ref(false);
+    import {
+        ref
+    } from "vue";
+    defineProps({
+        imageSrc: String,
+        altText: String,
+        title: String,
+        description: String,
+    });
+    const showDetails = ref(false);
 </script>
 
 <style scoped>
-/* Add any necessary styling */
+    /* Add any necessary styling */
 </style>
