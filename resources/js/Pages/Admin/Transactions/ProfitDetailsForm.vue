33f93f18d29d986f3489f8f112c17580c7abbd1f
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2024-02-12 00:43:46 +0200

    Update user authentication logic

--- resources/js/Pages/Admin/Transactions/ProfitDetailsForm.vue
+++ resources/js/Pages/Admin/Transactions/ProfitDetailsForm.vue
@@ -69,22 +69,18 @@ const resetForm = () => {
     Object.keys(form.errors).forEach((key) => {
         delete form.errors[key];
     });
-
-
 };
 
-const removeExtraServices = (index) => {
-    form.extra_services = form.extra_services.filter(
-        (item, i) => i !== index
-    );
-};
 
 const addFormItem = () => {
     form.extra_services.push({
         name: "",
         price: "",
         quantity: "",
-        details: "",
+        details: [
+            type = "",
+            originPrice = "",
+        ],
     });
 };
 
@@ -111,7 +107,6 @@ const submit = () => {
             Object.keys(form.errors).forEach((key) => {
                 delete form.errors[key];
             });
-
             Object.assign(form.errors, errors);
         },
     };
@@ -135,42 +130,52 @@ const submit = () => {
         })
     );
 };
-const showServiceTableDataId = ref(null);
-const showServiceTableData = (id) => {
-    showServiceTableDataId.value = id;
-};
+
+// extra services where has type = service
+const extraServicesService = computed(() => {
+    return form.extra_services.filter((service) => service.type === "service");
+});
 
 const calculateServiceProfit = (service) => {
     return (service.price * service.quantity) - (service.details * service.quantity);
 };
 
-const calculateExtraServiceProfit = (extraService) => {
-    return (extraService.price * extraService.quantity) - (extraService.details * extraService.quantity);
+const calculateExtraServiceProfit = (service) => {
+    return (service.price * service.quantity) - (service.details['originPrice'] * service.quantity);
 };
 
-// export default {
-//     name: "blueGray-tabs",
-//     data() {
-//         return {
-//             openServiceTab: 1
-//         }
-//     },
-//     methods: {
-//         toggleTabs: function (tabNumber) {
-//             this.openServiceTab = tabNumber
-//         }
-//     }
-// }
+const calculateWorkerProfit = (service) => {
+    return (service.price * service.quantity) - (service.details['originPrice'] * service.quantity);
+};
+
+// filteredServices
+const filteredServices = computed(() => {
+    return form.extra_services?.filter(item => item.type === 'service');
+});
+
+// filter worker
+const filteredWorkers = computed(() => {
+    return form.extra_services?.filter(item => item.type === 'worker');
+});
 
 const openServiceTab = ref(1);
 const toggleServiceTabs = (tabNumber) => {
     openServiceTab.value = tabNumber;
 };
 
-const openExtraServiceTab = ref(1);
+const openExtraServiceTab = ref("");
 const toggleExtraServiceTabs = (tabNumber) => {
     openExtraServiceTab.value = tabNumber;
 };
+
+// const firstWorkerId = form.extra_services[0]?.id; there is no 0
+// just use the first id from the array of workers couse it filtered so maybe the first one is not the first one in the array
+
+const openWorkerTab = ref("");
+const toggleWorkerTabs = (tabNumber) => {
+    openWorkerTab.value = tabNumber;
+};
+
 
 
 
@@ -313,15 +318,16 @@ const toggleExtraServiceTabs = (tabNumber) => {
         <label class="block mb-2 font-bold">
             الخدمات الاضافية :
             <span class="text-red-700">
-                {{ form.extra_services?.length }}
+                {{ filteredServices?.length }}
             </span>
         </label>
 
         <ul class="flex flex-row flex-wrap pt-3 pb-4 mb-0 list-none">
-            <li v-for="( formItem, index ) in  form.extra_services " :key="index" class="flex-auto mr-2 -mb-px text-center">
+
+            <li v-for="(formItem, index) in filteredServices" :key="index" class="flex-auto mr-2 -mb-px text-center">
                 <a class="block px-5 py-3 text-xs font-bold leading-normal uppercase rounded shadow-lg"
                     v-on:click="toggleExtraServiceTabs(formItem.id)"
-                    v-bind:class="{ 'text-blueGray-600 font-medium bg-gray-100 cursor-pointer': openExtraServiceTab !== formItem.id, 'text-white font-bold bg-gray-700': openExtraServiceTab === formItem.id }">
+                    v-bind:class="{ 'text-blueGray-600 font-medium bg-gray-100 cursor-pointer': openExtraServiceTab !== formItem.id, 'text-white font-bold bg-gray-700': (openExtraServiceTab ? openExtraServiceTab : filteredServices[0]?.id) === formItem.id }">
                     {{ formItem.name }}
                 </a>
             </li>
@@ -329,8 +335,10 @@ const toggleExtraServiceTabs = (tabNumber) => {
         <div class="relative flex flex-col w-full min-w-0 mb-6 break-words bg-white rounded shadow-lg">
             <div class="flex-auto px-4 py-5">
                 <div class="tab-content tab-space">
-                    <div v-for="( formItem, index ) in  form.extra_services " :key="index"
-                        v-bind:class="{ 'hidden': openExtraServiceTab !== formItem.id, 'block': openExtraServiceTab === formItem.id }">
+                    <div v-for="( formItem, index ) in  filteredServices " :key="index" v-bind:class="{
+                        'hidden': (openExtraServiceTab ? openExtraServiceTab : filteredServices[0]?.id)
+                            !== formItem.id, 'block': openExtraServiceTab === formItem.id
+                    }">
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead>
                                 <tr>
@@ -375,23 +383,25 @@ const toggleExtraServiceTabs = (tabNumber) => {
                         <h1 class="font-bold text-cyan-700">حساب الارباح :</h1>
                         <div class="grid grid-flow-row grid-cols-2">
                             <FormField :label="'سعر المتر في المادة الخام :'">
-                                <FormControl v-model="formItem.details" placeholder="سعر المتر في المادة الخام" />
+                                <FormControl v-model="formItem.details['originPrice']"
+                                    placeholder="سعر المتر في المادة الخام" />
+                                <!-- <input type="hidden" v-model="formItem.type" value="employ" /> -->
                             </FormField>
 
-                            <p class="m-auto" v-if="calculateServiceProfit(formItem) > 0">
+                            <p class="m-auto" v-if="calculateExtraServiceProfit(formItem) > 0">
                                 <span class="block ">
                                     المجموع : (سعر المتر الخام * عدد الامتار)=
                                     <strong class="font-bold text-red-700">
-                                        {{ formItem.details * formItem.quantity }}
+                                        {{ formItem.details['originPrice'] * formItem.quantity }}
                                         دينار
                                     </strong>
                                 </span>
-                                <span v-if="formItem.details" class="block font-bold text-green-700">
-                                    الربح : {{ calculateServiceProfit(formItem) }} دينار
+                                <span v-if="formItem.details['originPrice']" class="block font-bold text-green-700">
+                                    الربح : {{ calculateExtraServiceProfit(formItem) }} دينار
                                 </span>
                             </p>
 
-                            <p class="m-auto" v-else-if="calculateServiceProfit(formItem) === 0">
+                            <p class="m-auto" v-else-if="calculateExtraServiceProfit(formItem) === 0">
                                 <span class="block font-bold text-red-700">
                                     لا يوجد ربح
                                 </span>
@@ -401,16 +411,188 @@ const toggleExtraServiceTabs = (tabNumber) => {
                                 <span class="block ">
                                     المجموع : (سعر المتر الخام * عدد الامتار)=
                                     <strong class="font-bold text-red-700">
-                                        {{ formItem.details * formItem.quantity }}
+                                        {{ formItem.details['originPrice'] * formItem.quantity }}
                                         دينار
                                     </strong>
                                 </span>
                                 <span class="block font-bold text-red-700">
-                                    الخسارة : {{ calculateServiceProfit(formItem) }} دينار
+                                    الخسارة : {{ calculateExtraServiceProfit(formItem) }} دينار
                                 </span>
                             </p>
                         </div>
+                        <input type="hidden" v-bind:value="'service'">
 
+                        <!-- <input type="hidden" v-model="formItem.details['type']" value="'service'" /> -->
+
+
+                        <div v-if="form && form.errors">
+                            <!-- <span v-if=" form?.errors['services.' + id + '.price']" class="block text-sm text-red-600">
+                    {{ form?.errors['services.' + id + '.price'] }}
+                </span>
+                <span v-if="form?.errors['services.' + id + '.quantity']" class="block text-sm text-red-600">
+                    {{ form?.errors['services.' + id + '.quantity'] }}
+                </span> -->
+                            <span
+                                v-if="form?.errors['services.' + id + '.price'] || form?.errors['services.' + id + '.quantity']"
+                                class="block text-sm text-red-600">
+                                يجب التاكد من ملأ جميع البيانات بالشكل الصحيح
+                            </span>
+                            <BaseButtons>
+                                <BaseButton @click="submit" type="submit" color="info" label="حفظ" />
+                            </BaseButtons>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div v-for="( formItem, index ) in  form.extra_services " :key="index">
+            <div class="mb-5" v-if="form && form.errors">
+                <!-- <span v-if="form?.errors['extra_services.' + index + '.name']" class="block text-sm text-red-600">
+                        {{ form?.errors['extra_services.' + index + '.name'] }}
+                    </span>
+                    <span v-if="form?.errors['extra_services.' + index + '.price']" class="block text-sm text-red-600">
+                        {{ form?.errors['extra_services.' + index + '.price'] }}
+                    </span>
+                    <span v-if="form?.errors['extra_services.' + index + '.quantity']" class="block text-sm text-red-600">
+                        {{ form?.errors['extra_services.' + index + '.quantity'] }}
+                    </span> -->
+                <span v-if="form?.errors['extra_services.' + index + '.name'] ||
+                    form?.errors['extra_services.' + index + '.price'] ||
+                    form?.errors['extra_services.' + index + '.quantity']
+                    " class="block text-sm text-red-600">
+                    يجب التاكد من ملأ جميع البيانات بالشكل الصحيح
+                </span>
+            </div>
+        </div>
+
+        <!-- <div class="flex justify-end">
+            <BaseButton type="button" :icon="mdiPlus" color="info" label="خدمات اضافية" @click="addFormItem" />
+        </div> -->
+        <BaseDivider />
+
+
+        <!-- <template #footer>
+            <BaseButtons>
+                <BaseButton @click="submit" type="submit" color="info" label="Submit" />
+            </BaseButtons>
+        </template> -->
+    </CardBox>
+
+    <BaseDivider :bold="true" />
+
+    <CardBox form @submit.prevent="submit" :customClass="'overflow-y-auto w-96'">
+        <label class="block mb-2 font-bold">
+            العمالة :
+            <span class="text-red-700">
+                {{ filteredWorkers?.length }}
+            </span>
+        </label>
+
+        <ul class="flex flex-row flex-wrap pt-3 pb-4 mb-0 list-none">
+            <li v-for="( formWorker, index ) in  filteredWorkers " :key="index" class="flex-auto mr-2 -mb-px text-center">
+                <a class="block px-5 py-3 text-xs font-bold leading-normal uppercase rounded shadow-lg"
+                    v-on:click="toggleWorkerTabs(formWorker.id)"
+                    v-bind:class="{ 'text-blueGray-600 font-medium bg-gray-100 cursor-pointer': openWorkerTab !== formWorker.id, 'text-white font-bold bg-gray-700': (openWorkerTab ? openWorkerTab : filteredWorkers[0]?.id) === formWorker.id }">
+                    {{ formWorker.name }}
+                </a>
+            </li>
+        </ul>
+        <div class="relative flex flex-col w-full min-w-0 mb-6 break-words bg-white rounded shadow-lg">
+            <div class="flex-auto px-4 py-5">
+                <div class="tab-content tab-space">
+                    <div v-for="( formWorker, index ) in  filteredWorkers " :key="index" v-bind:class="{
+                        'hidden': (openWorkerTab ? openWorkerTab : filteredWorkers[0]?.id) !== formWorker.id, 'block': openWorkerTab === formWorker.id
+                    }">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead>
+                                <tr>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase bg-gray-50">
+                                        اليومية
+                                    </th>
+
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase bg-gray-50">
+                                        اكرامية
+                                    </th>
+
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase bg-gray-50">
+                                        خصم
+                                    </th>
+
+                                    <th scope="col"
+                                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase bg-gray-50">
+                                        التــريح
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr>
+                                    <!-- total -->
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ formWorker.price * formWorker.quantity }} دينار
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ formWorker.details['tips'] }} دينار
+                                    </td>
+
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ formWorker.details['discount'] }} دينار
+                                    </td>
+                                    <!-- created at -->
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ formWorker.created_at }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br>
+                        <h1 class="font-bold text-cyan-700">حساب الارباح :</h1>
+                        <div class="grid grid-flow-row grid-cols-2">
+                            <FormField :label="'اليومية :'">
+                                <FormControl v-model="formWorker.details['originPrice']" placeholder="اليومية" />
+                            </FormField>
+
+                            <p class="m-auto" v-if="calculateWorkerProfit(formWorker) > 0">
+                                <span class="block ">
+                                    <strong class="font-bold text-red-700">
+                                        {{ formWorker.details['originPrice'] * formWorker.quantity }}
+                                        دينار
+                                    </strong>
+                                </span>
+                                <span v-if="formWorker.details['originPrice']" class="block font-bold text-green-700">
+                                    الربح : {{ calculateWorkerProfit(formWorker) }} دينار
+                                </span>
+                            </p>
+
+                            <p class="m-auto" v-else-if="calculateWorkerProfit(formWorker) === 0">
+                                <span class="block font-bold text-red-700">
+                                    لا يوجد ربح
+                                </span>
+                            </p>
+
+                            <p class="m-auto" v-else>
+                                <span class="block ">
+                                    المجموع : (سعر المتر الخام * عدد الامتار)=
+                                    <strong class="font-bold text-red-700">
+                                        {{ formWorker.details['originPrice'] * formWorker.quantity }}
+                                        دينار
+                                    </strong>
+                                </span>
+                                <span class="block font-bold text-red-700">
+                                    الخسارة : {{ calculateWorkerProfit(formWorker) }} دينار
+                                </span>
+                            </p>
+                        </div>
+                        <input type="hidden" v-bind:value="'worker'">
+
+                        <!-- form worker -->
+                        <!-- formWorker.details['type'] == service  // hidden -->
+                        <!-- <FormField :label="'نوع العمالة :'">
+                            <FormControl v-model="formWorker.details['type']" placeholder="نوع العمالة" />
+                        </FormField> -->
 
                         <div v-if="form && form.errors">
                             <!-- <span v-if=" form?.errors['services.' + id + '.price']" class="block text-sm text-red-600">
