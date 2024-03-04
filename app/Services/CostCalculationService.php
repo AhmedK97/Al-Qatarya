4898149d0bf68a42d7c4bb0b2077389a58fe1e87
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2024-03-05 01:16:22 +0200

    Refactor cost calculation and profit calculation methods

--- app/Services/CostCalculationService.php
+++ app/Services/CostCalculationService.php
@@ -22,22 +22,20 @@ class CostCalculationService
     {
         $materialsCost = 0;
         foreach ($transaction->project->services as $service) {
-            $originPrice = json_decode($service->pivot->details)->originPrice;
-            $materialsCost += $originPrice * $service->pivot->quantity;
+            $originPrice = json_decode($service->pivot->details)->originPrice ?? 0;
+            $materialsCost += $originPrice * $service->pivot->quantity ?? 0;
         }
 
         foreach ($transaction->project->extraServices as $extraService) {
             if ($extraService->type == 'service') {
-                $originPrice = json_decode($extraService->details)->originPrice;
-                $materialsCost += $originPrice * $extraService->quantity;
+                $originPrice = json_decode($extraService->details) ?? 0;
+                $materialsCost += $originPrice * $extraService->quantity ?? 0;
             }
         }
 
         return $materialsCost;
     }
 
-
-    // worker
     public function calculateWorkerCost($transaction)
     {
         $workerCost = 0;
@@ -56,14 +54,14 @@ class CostCalculationService
     {
         $totalProfit = 0;
         foreach ($transaction->project->services as $service) {
-            $originPrice = json_decode($service->pivot->details)->originPrice;
-            $totalProfit += $originPrice * $service->pivot->quantity;
+            $originPrice = json_decode($service->pivot?->details)->originPrice ?? 0;
+            $totalProfit += $originPrice * $service->pivot?->quantity ?? 0;
         }
 
         foreach ($transaction->project->extraServices as $extraService) {
             if ($extraService->type == 'service') {
-                $originPrice = json_decode($extraService->details)->originPrice;
-                $totalProfit += $originPrice * $extraService->quantity;
+                $originPrice = json_decode($extraService->details) ?? 0;
+                $totalProfit += $originPrice * $extraService->quantity ?? 0;
             }
             if ($extraService->type == 'worker') {
                 foreach (json_decode($extraService->details) as $worker) {
@@ -76,5 +74,90 @@ class CostCalculationService
         $profit = $totalCost - $totalProfit;
 
         return $profit;
+    }
+
+    public function calculateServicesCost($transactions)
+    {
+        $totalCost = 0;
+        foreach ($transactions->project->services as $service) {
+            $totalCost += $service->pivot->price * $service->pivot->quantity;
+        }
+
+        return $totalCost;
+    }
+
+    public function calculateExtraServicesCost($transactions)
+    {
+        $totalCost = 0;
+        foreach ($transactions->project->extraServices as $extraService) {
+            $totalCost += $extraService->price * $extraService->quantity;
+        }
+
+        return $totalCost;
+    }
+
+
+    public function calculateServicesProfit($transaction)
+    {
+        $totalProfit = 0;
+        foreach ($transaction->project->services as $service) {
+            $originPrice = json_decode($service->pivot->details)->originPrice ?? 0;
+            $price = $service->pivot->price ?? 0;
+            $totalProfit += $price * $service->pivot->quantity - $originPrice * $service->pivot->quantity;
+        }
+
+
+
+        return $totalProfit;
+    }
+
+    public function calculateExtraServiceProfit($transaction)
+    {
+        $totalProfit = 0;
+
+        foreach ($transaction->project->extraServices as $extraService) {
+            if ($extraService->type == 'service') {
+                $originPrice = json_decode($extraService->details) ?? 0;
+                $price = $extraService->price ?? 0;
+                $totalProfit += $price * $extraService->quantity - $originPrice * $extraService->quantity;
+            }
+        }
+
+        return $totalProfit;
+    }
+
+
+    ////////////////////////
+
+
+    public function calculateAllServicesProfit($transactions)
+    {
+        $totalProfit = 0;
+        foreach ($transactions as $transaction) {
+            $totalProfit += $this->calculateServicesProfit($transaction);
+        }
+
+        return $totalProfit;
+    }
+
+    public function calculateAllExtraServicesProfit($transactions)
+    {
+        $totalProfit = 0;
+        foreach ($transactions as $transaction) {
+            $totalProfit += $this->calculateExtraServiceProfit($transaction);
+        }
+
+        return $totalProfit;
+    }
+
+    // worker cost
+    public function calculateAllWorkerCost($transactions)
+    {
+        $totalCost = 0;
+        foreach ($transactions as $transaction) {
+            $totalCost += $this->calculateWorkerCost($transaction);
+        }
+
+        return $totalCost;
     }
 }
