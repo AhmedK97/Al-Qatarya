f86421f361bbb6b8b4f10f40c1cd92434d62b4ae
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2024-04-02 22:10:02 +0200

    Update user password and phone number in UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,8 +27,10 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             // 'email' => $this->faker->unique()->safeEmail(),
             // 'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'phone' => '01126110083',
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // password 11111111
+            'password' => decrypt('11111111'),
+            'phone' => '11111111',
             'status' => $this->faker->randomElement(EmployeeStatusEnum::cases()),
             'address' => $this->faker->address(),
             'about' => $this->faker->paragraph(),
