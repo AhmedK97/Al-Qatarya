3959a2e50d805a6578c10ef8fdb4f5e00500a927
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2024-04-12 04:52:17 +0200

    IndexWhatsAppAdsController counter

--- app/Http/Controllers/IndexWhatsAppAdsController.php
+++ app/Http/Controllers/IndexWhatsAppAdsController.php
@@ -31,7 +31,7 @@ class IndexWhatsAppAdsController extends Controller
         return Inertia::render('Admin/WhatsAppAds/Index', [
             'whatsAppAdsActiveNumbers' => $whatsAppAdsActiveNumbers,
             'whatsAppAdsData' => $waData,
-            'whatsAppAdsDataCount' => $waData->where('status', 'sent')->count(),
+            'whatsAppAdsDataCount' => WhatsApp::where('status', 'sent')->count(),
             'queueRunning' => \DB::table('jobs')->where('queue', 'default')->where('payload', 'like', '%SendMessageJob%')->exists(),
         ]);
     }
