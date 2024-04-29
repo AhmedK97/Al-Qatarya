266ef450b13c1d69dfdc090b7e2740055e2ff58f
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2024-04-29 17:43:38 +0300

    Fix calculation issues and remove unnecessary code in TransactionsAdminResource.php

--- app/Http/Resources/TransactionsAdminResource.php
+++ app/Http/Resources/TransactionsAdminResource.php
@@ -15,43 +15,43 @@ class TransactionsAdminResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $costCalculationService = resolve(CalculationService::class);
+        // $costCalculationService = resolve(CalculationService::class);
 
-        $allServicesCost = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateTotalCost($this->resource);
-        });
+        // $allServicesCost = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateTotalCost($this->resource);
+        // });
 
-        $materialsCost = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateMaterialsCost($this->resource);
-        });
+        // $materialsCost = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateMaterialsCost($this->resource);
+        // });
 
-        $profit = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateProfit($this->resource);
-        });
+        // $profit = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateProfit($this->resource);
+        // });
 
-        $workerCost = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateWorkerCost($this->resource);
-        });
+        // $workerCost = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateWorkerCost($this->resource);
+        // });
 
-        $servicesCost = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateServicesCost($this->resource);
-        });
+        // $servicesCost = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateServicesCost($this->resource);
+        // });
 
-        $extraServicesCost = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateExtraServicesCost($this->resource);
-        });
+        // $extraServicesCost = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateExtraServicesCost($this->resource);
+        // });
 
-        $ServiceProfit = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateServicesProfit($this->resource);
-        });
+        // $ServiceProfit = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateServicesProfit($this->resource);
+        // });
 
-        $ExtraServiceProfit = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateExtraServiceProfit($this->resource);
-        });
+        // $ExtraServiceProfit = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateExtraServiceProfit($this->resource);
+        // });
 
-        $totalPaid = $this->whenLoaded('project', function () use ($costCalculationService) {
-            return $costCalculationService->calculateTotalPaid($this->resource);
-        });
+        // $totalPaid = $this->whenLoaded('project', function () use ($costCalculationService) {
+        //     return $costCalculationService->calculateTotalPaid($this->resource);
+        // });
 
         return [
             'id' => $this->id,
@@ -92,17 +92,17 @@ class TransactionsAdminResource extends JsonResource
                 return ServicesResource::collection($this->project->services);
             }),
             'payments' => json_decode($this->payments),
-            'total_paid' => $totalPaid,
-            'status' => $this->status,
-            'times_to_pay' => $this->times_to_pay,
-            'all_services_cost' => $allServicesCost,
-            'materials_cost' => $materialsCost,
-            'worker_cost' => $workerCost,
-            'services_cost' => $servicesCost,
-            'extra_services_cost' => $extraServicesCost,
-            'service_profit' => $ServiceProfit,
-            'extra_service_profit' => $ExtraServiceProfit,
-            'profit' => $profit,
+            // 'total_paid' => $totalPaid,
+            // 'status' => $this->status,
+            // 'times_to_pay' => $this->times_to_pay,
+            // 'all_services_cost' => $allServicesCost,
+            // 'materials_cost' => $materialsCost,
+            // 'worker_cost' => $workerCost,
+            // 'services_cost' => $servicesCost,
+            // 'extra_services_cost' => $extraServicesCost,
+            // 'service_profit' => $ServiceProfit,
+            // 'extra_service_profit' => $ExtraServiceProfit,
+            // 'profit' => $profit,
 
 
             'created_at' => $this->created_at->format('Y-m-d'),
