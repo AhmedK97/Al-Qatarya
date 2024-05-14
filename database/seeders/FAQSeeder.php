b60c0fd738c43aed079c15b04a3116abdfc717c1
Ahmed Moustafa <30881400+AhmedK97@users.noreply.github.com>
2024-05-14 20:40:58 +0300

    fix

--- database/seeders/FAQSeeder.php
+++ database/seeders/FAQSeeder.php
@@ -12,6 +12,33 @@ class FAQSeeder extends Seeder
      */
     public function run(): void
     {
-        FAQ::factory(16)->create(); // Creates 30 dummy FAQ entries
+
+        FAQ::truncate();
+
+        FAQ::create([
+            'question' => 'What is the capital of France?',
+            'answer' => 'Paris',
+            'locale' => 'en',
+        ]);
+
+        FAQ::create([
+            'question' => 'What is the capital of Egypt?',
+            'answer' => 'Cairo',
+            'locale' => 'en',
+        ]);
+
+        FAQ::create([
+            'question' => 'ما هو عاصمة فرنسا؟',
+            'answer' => 'باريس',
+            'locale' => 'ar',
+        ]);
+
+        FAQ::create([
+            'question' => 'ما هو عاصمة مصر؟',
+            'answer' => 'القاهرة',
+            'locale' => 'ar',
+        ]);
+
+
     }
 }
